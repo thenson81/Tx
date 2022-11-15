d9ce2d43a3c1bbdf7d923572c460d7b8e2406de1
Thomas Henson <thenson81@gmail.com>
2022-11-15 17:57:05 -0500

    Adding testing and changin up build configs

---
+++ Source/ConsoleAppSdkTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleAppSdkTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo schema = new FileInfo(@"C:\Rms\1109\BothLogs\schema2.man");
+            var manifest = File.ReadAllText(schema.FullName);
+            var manifestDictionary = Tx.Windows.ManifestParser.Parse(manifest);
+            Dictionary<string, string> generated = new Dictionary<string, string>();
+        }
+    }
+}
